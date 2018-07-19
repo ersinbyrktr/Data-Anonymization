bc2e911e7ec177110ac41f46806798096bcc44e8
Ersin Bayraktar <ersinbyrktr@gmail.com>
2018-07-20 01:05:33 +0200

    Removed Query File

--- src/main/java/org/anonymization/examples/ExampleMongo.java
+++ src/main/java/org/anonymization/examples/ExampleMongo.java
@@ -1,19 +1,11 @@
 package org.anonymization.examples;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.CreateCollectionOptions;
 import org.anonymization.repository.MongoDBService;
-import org.bson.Document;
 import org.deidentifier.arx.ARXResult;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Scanner;
 
 import static org.anonymization.examples.Example.*;
 
@@ -33,43 +25,21 @@ public class ExampleMongo {
 
         service.connect();
 
-        insertExampleData(service);
-
         service.executeQuery();
 
         ARXResult result = getResult();
         processResults(result);
     }
 
-    private static void insertExampleData(MongoDBService service) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        MongoDatabase db = service.getDB();
-        MongoCollection<Document> coll = db.getCollection(collectionName);
-        coll.drop();
-        createCollection(db);
-
-        JsonNode masterJSON = mapper.readTree(new File(
-                Objects.requireNonNull(ExampleMongo.class.getClassLoader().getResource("exampleMongoDBData.json"))
-                        .getFile()
-        ));
-
-        masterJSON.iterator()
-                .forEachRemaining(e -> coll.insertOne(Document.parse(e.toString())));
-    }
-
-    private static void createCollection(MongoDatabase db) {
-        db.createCollection(collectionName, new CreateCollectionOptions());
-    }
-
     private static String getExampleQuery() {
-        try {
-            return new Scanner(new File(
-                    Objects.requireNonNull(ExampleMongo.class.getClassLoader().getResource("exampleMongoDBQuery.json"))
-                            .getFile()
-            )).useDelimiter("\\Z").next();
-        } catch (FileNotFoundException | NullPointerException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return "{\n" +
+                "  \"$project\":{\n" +
+                "    \"name\":1,\n" +
+                "    \"age\":1,\n" +
+                "    \"disease\":1,\n" +
+                "    \"nationality\":1,\n" +
+                "    \"zip\":\"$address.zip\"\n" +
+                "  }\n" +
+                "}";
     }
 }
